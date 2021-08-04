cabfe9026b9a7469b06c4588aaff42b6793315ab
Tanzim395 <junaidulkader@gmail.com>
2021-08-04 11:48:39 +0600

    first commit

---
+++ function.js
@@ -0,0 +1,20 @@
+// function declaration
+function startFan() {
+    console.log('walk towards the switch')
+    console.log('press the switch')
+}
+// call the function
+startFan();
+
+console.log('eat breakfast');
+console.log('Drink tea');
+console.log('take a shower');
+// startFan();
+var country = "Bangladesh";
+var city = 'Dhaka';
+var location = 'Gulisthan';
+startFan();
+var sleep = 'Go to Sleep';
+console.log(sleep);
+startFan();
+console.log('Get Up from the Bed');
---
+++ temp.js
@@ -0,0 +1,3 @@
+var nums = [4, 5, 7, 6, 8, 2, 4, 1, 15];
+var numIndex = nums.indexOf(6);
+console.log(numIndex);
